a5396d89d197b754e9bf025da9e886a28224210d
Mohammad Mahdi Ebrahim Soltani <102657542+MahdiES0@users.noreply.github.com>
2023-11-17 08:41:56 -0800

    Update checkOnes.v

--- checkOnes.v
+++ checkOnes.v
@@ -1,4 +1,4 @@
-module checkOnes(input p0, p1, p2, p3, output reg found);
+module checkOnes(input p0, p1, p2, p3, output reg found, out);
     always @(*)begin
         case({p0, p1, p2, p3})
             4'b1110: found = 1'b1;
