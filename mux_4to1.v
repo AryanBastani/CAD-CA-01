31b9a161e93d6d181eaf828b8f5280e4d387b7b8
Mohammad Mahdi Ebrahim Soltani <102657542+MahdiES0@users.noreply.github.com>
2023-11-17 18:31:24 +0330

    Add files via upload
    mux 4 to 1

---
+++ mux_4to1.v
@@ -0,0 +1,12 @@
+module mux_4to1(input [31:0] in0, in1, in2, in3, input [1:0] sel, output reg [31:0] out);
+
+always @(*) begin
+    case (sel)
+        2'b00: out = in0;
+        2'b01: out = in1;
+        2'b10: out = in2;
+        2'b11: out = in3;
+    endcase
+end
+
+endmodule
