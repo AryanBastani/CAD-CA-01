e472420c4041e1e22b0540198b75e246ba6b75e4
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2023-11-18 11:53:18 +0330

    Fix and edit controller.v

--- controller.v
+++ controller.v
@@ -18,70 +18,46 @@ module controller (
         state3 = 3'b011,
         state4 = 3'b100,
         state5 = 3'b101,
-        state6 = 3'b110;
+        state6 = 3'b110,
+	    state7 = 3'b111;
 
     reg [2:0] ps = state0;
     reg [2:0] ns;
 
-    always @(ps, start, found) begin
+    always@(ps or start or found)
+    begin
         ns = state0;
+        case (ps)
+            state0: ns = start ? state1 : state0;
+            state1: ns = state2;
+            state2: ns = state3;
+            state3: ns = state4;
+            state4: ns = state5;
+            state5: ns = found ? state7 : state6;
+            state6: ns = state3;
+            state7: ns = state0;
+        endcase
+    end
+
+    always @(ps) begin
         {done, actWrite, multWrite, mainRegWrite, addWrite} = 5'b0;
         case (ps)
-            state0: begin 
-                if(start) begin
-                    mainRegWrite = 1;
-                    ns = state1;
-                end
-                else begin
-                    ns = state0;
-                end 
-            end 
-            state1: begin
-                s1 = 0;
-                s2 = 0;
-                s3 = 0;
-                s4 = 0;
-                actWrite = 1;
-                ns = state2;
-            end
-            state2: begin
-                multWrite = 1;
-                ns = state3;
-            end
-            state3: begin
-                addWrite = 1;
-                ns = state4;
-            end
-            state4: begin
-                if (found)begin
-                    ns = state6;
-                end
-                else begin
-                    ns = state5;
-                end
-            end
-            state5: begin
-                s1 = 1;
-                s2 = 1;
-                s3 = 1;
-                s4 = 1;
-                actWrite = 1;
-                ns = state2;
-            end
-            state6: begin
-                done = 1;
-                if (start)begin
-                    ns = state1;
-                end
-                else begin
-                    ns = state0;
-                end
-            end
+            state0: ;
+            state1: mainRegWrite = 1;
+            state2: {s1, s2, s3, s4, actWrite} = 5'b00001;
+            state3: multWrite = 1;
+            state4: addWrite = 1;
+            state5: ;
+            state6: {s1, s2, s3, s4, actWrite} = 5'b11111;
+            state7: done = 1;
         endcase
     end
     
-    always @(posedge clk) begin
-        ps <= ns;
+    always @(posedge clk, posedge rst) begin
+        if(rst)
+            ps <= state0;
+        else
+            ps <= ns;
     end
     
 endmodule
