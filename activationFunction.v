b11281ff68d2ef970e0beef5a3b69a5fa296ac08
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2023-11-18 15:54:43 +0330

    Fix activationFunction.v

--- activationFunction.v
+++ activationFunction.v
@@ -1,7 +1,7 @@
 module activationFunction(input [31:0] in, output reg [31:0] out);
 
 always @(*) begin
-    if (in <= 0) begin
+    if (in[31]) begin
         out = 32'b0;
     end else begin
         out = in;
