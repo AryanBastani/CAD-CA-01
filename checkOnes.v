bbf8b9d1a3e22985d55873a5799d89e92dddcd10
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2023-11-18 09:06:36 +0330

    Fix checkOnes.v

--- checkOnes.v
+++ checkOnes.v
@@ -1,11 +1,11 @@
-module checkOnes(input p0, p1, p2, p3, output reg found, out);
+module checkOnes(input p0, p1, p2, p3, output reg found);
     always @(*)begin
         case({p0, p1, p2, p3})
             4'b1110: found = 1'b1;
             4'b1101: found = 1'b1;
             4'b1011: found = 1'b1;
             4'b0111: found = 1'b1;
-            default: out = 1'b0;  
+            default: found = 1'b0;  
         endcase
     end
 endmodule
