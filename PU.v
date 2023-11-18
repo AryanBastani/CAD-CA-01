ce3b17209d66e3437d6e56974590cc7fbb21eaef
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2023-11-18 08:51:25 +0330

    Update PU.v

--- PU.v
+++ PU.v
@@ -11,17 +11,17 @@ module PU(
         add_in1, add_in2, add_in3, add_in4,
         add_out1, add_out2;
 
-    floating_multiplier multi1(x1, w1, mult_out1);
-    floating_multiplier multi2(x2, w2, mult_out2);
-    floating_multiplier multi3(x3, w3, mult_out3);
-    floating_multiplier multi4(x4, w4, mult_out4);
+    fp_multiplier multi1(x1, w1, mult_out1);
+    fp_multiplier multi2(x2, w2, mult_out2);
+    fp_multiplier multi3(x3, w3, mult_out3);
+    fp_multiplier multi4(x4, w4, mult_out4);
 
     group_4_registers mult_registers(clk, rst, mult_write,
         mult_out1, mult_out2, mult_out3, mult_out4,
         add_in1, add_in2, add_in3, add_in4);
 
-    floating_adder adder1_1(add_in1, add_in2, add_out1);
-    floating_adder adder1_2(add_in3, add_in4, add_out2);
+    fp_adder adder1_1(add_in1, add_in2, add_out1);
+    fp_adder adder1_2(add_in3, add_in4, add_out2);
 
-    floating_adder adder2_1(add_out1, add_out2, a);
+    fp_adder adder2_1(add_out1, add_out2, a);
 endmodule
